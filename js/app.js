35ff295e88eca26ab56f01efa941f04515ef4027
Jurgen Stevens <southsidejdot@Jurgens-MacBook-Air.local>
2019-10-07 00:15:09 -0500

    Controls adjusted

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
 console.log('This is Hillbomber');
+
+class Skater {
+	constructor(name) {
+		this.name = name;
+		this.speed = null;
+		this.wobble = null;
+	}
+}
+
+
+// This 
+// const canvas = document.getElementById('canvas');
+// const ctx = canvas.getContext('2d');
+
+// ctx.fillStyle = 'black';
+// ctx.fillRect(600, 600, 25, 70);	
+
+// const hillbomberGame = {
+
+// 	hillBomber: null,
+// 	time: 0,
+
+
+// 	makeHillbomber(){
+
+// 	},
+
+
+
+
+
+// }
+
+
+// Listeners!
